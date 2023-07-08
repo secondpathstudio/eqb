669c2a7e1f9031a18542661ca7b4a1da17e235a1
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2023-07-08 14:29:07 -0700

    added colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "eqb-card-bg": "#003049",
+        "eqb-bg-dark": "#D62828",
+        "eqb-bg-light": "#F77F00",
+        "eqb-accent": "#FCBF49",
+        "eqb-text": "#EAE2B7",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
