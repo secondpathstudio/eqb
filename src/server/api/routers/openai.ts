2985fe5361193f0de74eaaec5cc46e8d83237049
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2023-07-08 22:01:04 -0700

    added upstash and finished generate question

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -8,17 +8,47 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { TRPCError } from "@trpc/server";
+
+//3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+})
+
 export const openaiRouter = createTRPCRouter({
   generateQuestion: privateProcedure
     .input(
       z.object({
-        specialty: z.string(),
+        topic: z.string(),
         trainingLevel: z.enum(["medicalStudent", "resident", "attending"]),
       })
     )
-    .mutation(async ({ctx, input}) => {
-      const userId = ctx.currentUser.id;
-      const prompt: string = `Create a question for a ${input.trainingLevel} studying for a ${input.specialty} exam.`
+    .mutation(async ({ctx, input: {topic, trainingLevel}}) => {
+      console.log('Generating Question')
+      const userId = ctx.currentUser;
+
+      const {success} = await ratelimit.limit(userId);
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+        });
+      }
+      
+      const prompt: string = `Create a question for a ${trainingLevel} studying for a ${topic} exam. Please include the question and 4 answers. 
+        Please make sure the question is appropriate for the training level. Please return the response in the following JSON format: 
+        {
+          "questionText": "What is the most common cause of death in the United States?",
+          "answers": [
+            {
+              "answerText": "Heart Disease",
+              "isCorrect": true
+            }
+          ]
+        }`
       
       const response = await openai.createChatCompletion({
         model: "gpt-4",
@@ -32,6 +62,16 @@ export const openaiRouter = createTRPCRouter({
         ]
       })
 
-        return response.data.choices[0]?.message?.content;
+      const jsonResponses = JSON.parse(response.data.choices[0]?.message?.content!)
+      const questionText = jsonResponses.questionText
+      const answers = jsonResponses.answers
+
+        return {
+          questionText: questionText,
+          answers: answers,
+          topic: topic,
+          trainingLevel: trainingLevel,
+          aiModelUsed: "gpt-4",
+        }
     })
 });
